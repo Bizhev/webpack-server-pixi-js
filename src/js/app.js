9e0e91429c1f31478701f85c61100021079e1ee0
Dolet B <doletbizhev@gmail.com>
2019-12-21 23:57:40 +0300

    upgrade packages

--- src/js/app.js
+++ src/js/app.js
@@ -9,30 +9,30 @@ class ParticleText {
 	addObjects(){
 		PIXI.loader.add('bunny', '../img/BMWi8.jpg').load((loader, resources) => {
 			this.bunny = new PIXI.Sprite(resources.bunny.texture);
-		this.bunny.texture.frame = new PIXI.Rectangle(132,132,132,132)
+			this.bunny.texture.frame = new PIXI.Rectangle(132,132,132,132);
 
-		// Setup the position of the bunny
-		this.bunny.x = this.app.renderer.width / 2;
-		this.bunny.y = this.app.renderer.height / 2;
-	
-		// Rotate around the center
-		this.bunny.anchor.x = 0.5;
-		this.bunny.anchor.y = 0.5;
-	
-		// Add the bunny to the scene we are building
-		this.app.stage.addChild(this.bunny);
-		this.animate();
-	});
-		
+			// Setup the position of the bunny
+			this.bunny.x = this.app.renderer.width / 2;
+			this.bunny.y = this.app.renderer.height / 2;
+
+			// Rotate around the center
+			this.bunny.anchor.x = 0.5;
+			this.bunny.anchor.y = 0.5;
+
+			// Add the bunny to the scene we are building
+			this.app.stage.addChild(this.bunny);
+			this.animate();
+		});
+
 	}
 	animate(){
 		// Listen for frame updates
 		this.app.ticker.add(() => {
 			// each frame we spin the bunny around a bit
 			this.bunny.rotation += 0.01;
-		});		
+		});
 	}
 }
 
-let pop = new ParticleText()
+let pop = new ParticleText();
 console.log('p',pop);
